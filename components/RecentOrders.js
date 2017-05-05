0e5f7ccd948120386cc8b1989bef8fb3763f712d
dcp12345678 <dcp12345678@yahoo.com>
2017-05-05 11:15:28 -0400

    Rework RecentOrders screen to improve look and feel
    Changed the buttons for 'Add new order' and 'search' to use just icons
    instead of buttons with text.
    Each order in the ListView is clickable, and clicking an order will expand
    it to show more of the details, like the number of each type of item purchased.
    Also included an edit button in the expanded view so the user can edit the
    order if needed.

--- components/RecentOrders.js
+++ components/RecentOrders.js
@@ -8,6 +8,8 @@ import {
   TouchableHighlight,
   Alert,
   Button,
+  Platform,
+  Dimensions,
 } from 'react-native';
 import data from '../data/sales.json';
 import OrdersApi from '../api/OrdersApi';
@@ -15,41 +17,23 @@ import _ from 'lodash';
 import Helper from '../helpers/Helper';
 import co from 'co';
 import Promise from 'bluebird';
+import MaterialIcon from 'react-native-vector-icons/MaterialIcons';
+import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome';
+import LinearGradient from 'react-native-linear-gradient';
 
 const basketIcon = require('../images/basket.png');
 const ordersApi = new OrdersApi();
+const width = Dimensions.get('window').width;
 
 const styles = StyleSheet.create({
-  mainContainer: {
-    flex: 1,
-    //backgroundColor: '#fff',
-  },
-  title: {
-    backgroundColor: '#0f1b29',
-    color: '#fff',
-    fontSize: 18,
-    fontWeight: 'bold',
-    padding: 10,
-    textAlign: 'center',
-  },
   row: {
     borderColor: '#f1f1f1',
     borderBottomWidth: 1,
     flexDirection: 'row',
     marginLeft: 10,
     marginRight: 10,
-    paddingTop: 20,
-    paddingBottom: 20
-  },
-  iconContainer: {
-    alignItems: 'center',
-    backgroundColor: '#feb401',
-    borderColor: '#feaf12',
-    borderRadius: 25,
-    borderWidth: 1,
-    justifyContent: 'center',
-    height: 50,
-    width: 50,
+    paddingTop: 5,
+    paddingBottom: 5
   },
   icon: {
     tintColor: '#fff',
@@ -67,7 +51,7 @@ const styles = StyleSheet.create({
     marginBottom: 5,
   },
   orderDetail: {
-    color: '#ccc',
+    color: 'darkblue',
     fontSize: 18,
   },
   total: {
@@ -76,6 +60,7 @@ const styles = StyleSheet.create({
   date: {
     fontSize: 15,
     marginBottom: 5,
+    color: 'darkblue'
   },
   price: {
     color: '#1cad61',
@@ -88,10 +73,8 @@ const styles = StyleSheet.create({
     justifyContent: 'space-around',
     marginTop: 10,
     marginBottom: 10,
+    backgroundColor: 'steelblue'
   },
-  button: {
-    width: 150
-  }
 });
 
 class RecentOrders extends Component {
@@ -101,45 +84,63 @@ class RecentOrders extends Component {
   }
 
   getOrders = () => {
-    // add delay to work around react-native bug when displaying modal:
-    // https://github.com/facebook/react-native/issues/10845
     let self = this;
-    setTimeout(() => {
-      co(function* () {
-        let res = yield ordersApi.getOrdersForUser(self.props.userId);
-        let orders = JSON.parse(res.text);
+    co(function* () {
+      let res = yield ordersApi.getOrdersForUser(self.props.userId);
+      let orders = JSON.parse(res.text);
 
-        for (let i = 0; i < orders.length; ++i) {
-          const o = orders[i];
-          let res = yield ordersApi.getOrderLineItems(o.id);
-          const orderLineItems = JSON.parse(res.text);
-          o.products = {};
-          _.map(orderLineItems, (i) => {
-            if (_.isUndefined(o.products[i.productTypeName])) {
-              o.products[i.productTypeName] = 1;
-            } else {
-              ++o.products[i.productTypeName];
-            }
-          });
-
-        }
-
-        let ds = new ListView.DataSource({
-          rowHasChanged: (r1, r2) => r1 !== r2
+      for (let i = 0; i < orders.length; ++i) {
+        const o = orders[i];
+        o.isExpanded = false;  // indicates whether order is expanded to include its details
+        let res = yield ordersApi.getOrderLineItems(o.id);
+        const orderLineItems = JSON.parse(res.text);
+        o.lineItems = orderLineItems;
+        o.products = {};
+        _.map(orderLineItems, (i) => {
+          if (_.isUndefined(o.products[i.productTypeName])) {
+            o.products[i.productTypeName] = 1;
+          } else {
+            ++o.products[i.productTypeName];
+          }
         });
-        self.setState({
-          dataSource: ds.cloneWithRows(orders),
-        });
-      }).catch((err) => {
-        debugger;
-        Alert.alert(`error getting orders, ${JSON.stringify(err)}`);
+
+      }
+
+      let ds = new ListView.DataSource({
+        rowHasChanged: (r1, r2) => r1 !== r2
       });
-    }, 1000);
+      self.setState({
+        dataSource: ds.cloneWithRows(orders),
+        orders
+      });
+    }).catch((err) => {
+      debugger;
+      Alert.alert(`error getting orders, ${JSON.stringify(err)}`);
+    });
   }
 
-  onPressButton = (orderId) => {
-    debugger;
-    //Alert.alert(`click, orderId = ${orderId}`);
+  orderOnPress = (orderId) => {
+    // update the data source, flipping the expanded state for the order matching passed in orderId
+
+    let newOrders = _.cloneDeep(this.state.orders);
+
+    _.forEach(newOrders, (order) => {
+      if (order.id === orderId) {
+        order.isExpanded = !order.isExpanded;
+      }
+    });
+
+    let ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2
+    });
+
+    this.setState({
+      dataSource: ds.cloneWithRows(newOrders),
+      orders: newOrders
+    });
+  }
+
+  viewOrderDetailsOnPress = (orderId) => {
     this.props.navigator.push({
       name: 'OrderDetails',
       passProps: {
@@ -152,46 +153,6 @@ class RecentOrders extends Component {
   constructor(props) {
     super(props);
     this.state = { dataSource: undefined };
-    //Alert.alert('inside RecentOrders constructor, props.userId = ' + props.userId);
-    // let ds = new ListView.DataSource({
-    //   rowHasChanged: (r1, r2) => r1 !== r2
-    // });
-    // this.state = {
-    //   dataSource: ds.cloneWithRows(data),
-    //   orders: undefined,
-    // };
-  }
-
-  renderRow = (record) => {
-    let numProducts = 0;
-    let products = '';
-    let newLine = '';
-    for (let key in record.products) {
-      products += newLine + key + ': ' + record.products[key];
-      ++numProducts;
-      newLine = '\n';
-    };
-
-    return (
-      <TouchableHighlight onPress={() => { this.onPressButton(record.id); }}>
-        <View>
-          <View style={styles.row}>
-            <View style={styles.iconContainer}>
-              <Image source={basketIcon} style={styles.icon} />
-            </View>
-            <View style={styles.info}>
-              <Text style={styles.items}>{record.lineItems.length} Items</Text>
-              <Text style={styles.orderDetail}>Order Id: {record.id}</Text>
-              <Text style={styles.orderDetail}>No. items purchased: {numProducts}</Text>
-              <Text style={styles.orderDetail}>{products}</Text>
-            </View>
-            <View style={styles.total}>
-              <Text style={styles.date}>Last Update:{'\n'}{Helper.formatDate(record.updateDate)}</Text>
-            </View>
-          </View>
-        </View>
-      </TouchableHighlight >
-    );
   }
 
   newOrderOnPress = () => {
@@ -205,6 +166,79 @@ class RecentOrders extends Component {
   }
 
   searchOrdersOnPress = () => {
+    Alert.alert('need to implement');
+  }
+
+  renderRow = (record) => {
+    let expandedContent;
+
+    // if isExpanded is true, we need to show additional information for this order
+    if (record.isExpanded) {
+      let products = [];
+      let content = [];
+      content.push(
+        <View key="view_expandedHeader" style={{ flexDirection: 'row' }}>
+          <Text key="expandedHeader" style={[styles.orderDetail, { fontWeight: '600' }]}>Items Purchased:</Text>
+
+          <View style={{ marginLeft: 30 }}>
+            <TouchableHighlight
+              style={{
+                borderRadius: 20,
+              }}
+              underlayColor='#578dba' onPress={() => { this.viewOrderDetailsOnPress(record.id); }}>
+              <FontAwesomeIcon name='edit' color='white' size={25}
+                style={
+                  {
+                    alignSelf: 'center',
+                  }
+                } />
+            </TouchableHighlight>
+          </View>
+
+        </View >
+      );
+      for (let key in record.products) {
+        products.push(
+          <View key={key} style={{ flex: 1, flexDirection: 'row' }}>
+            <View style={
+              {
+                flex: .5,
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                borderWidth: 1,
+                borderColor: 'white',
+                backgroundColor: 'steelblue',
+                margin: 1,
+              }}
+              key={key}>
+              <Text key={key + '_text'} style={{ flex: .6, color: 'white', borderWidth: 1, borderColor: 'white', padding: 2 }}>{key}:</Text>
+              <Text key={key + '_cnt'} style={{ flex: .4, color: 'white', borderWidth: 1, borderColor: 'white', padding: 2 }}>{record.products[key]}</Text>
+            </View>
+            <View style={{ flex: .5 }}>
+            </View>
+          </View>
+        );
+      };
+      content.push(<View key="expandedDetail" style={{ marginBottom: 8, }}>{products}</View>);
+      expandedContent = (<View>{content}</View>);
+    }
+
+    return (
+      <TouchableHighlight underlayColor='#578dba' onPress={() => { this.orderOnPress(record.id); }}>
+        <View>
+          <View style={styles.row}>
+            <View style={[styles.info, { backgroundColor: '#5282aa', borderRadius: 9, }]}>
+              <View style={{ flexDirection: 'row' }}>
+                <Text style={styles.orderDetail}>Order: {record.id}</Text>
+                <Text style={{ marginLeft: 5, color: 'white', alignSelf: 'center'}}>({record.lineItems.length} items)</Text>
+              </View>
+              <Text style={styles.date}>Last Update: {Helper.formatDate(record.updateDate)}</Text>
+              {expandedContent}
+            </View>
+          </View>
+        </View>
+      </TouchableHighlight >
+    );
   }
 
   render() {
@@ -212,24 +246,42 @@ class RecentOrders extends Component {
       return <View></View>;
     }
     return (
-      <View style={styles.mainContainer}>
-        <View style={styles.buttonContainer}>
-          <View style={styles.button}>
-            <Button
-              onPress={this.newOrderOnPress}
-              title="New Order"
-              color='#841584'
-            />
-          </View>
-          <View style={styles.button}>
-            <Button
-              onPress={this.searchOrdersOnPress}
-              title="Search Orders"
-              color='#841584'
-            />
-          </View>
+      <View style={{ flex: 1, flexDirection: 'column' }}>
+        <View style={{
+          flexDirection: 'row', justifyContent: 'space-between', alignSelf: 'stretch', backgroundColor: 'steelblue',
+          marginBottom: 5,
+        }}>
+          <TouchableHighlight
+            style={{
+              marginTop: (Platform.OS === 'ios') ? 25 : 5,
+              borderRadius: 20,
+            }}
+            underlayColor='#578dba' onPress={this.newOrderOnPress}>
+            <MaterialIcon name='add-circle-outline' color='white' size={25} style={{ alignSelf: 'center', marginLeft: 5, marginTop: 5, marginBottom: 5, marginRight: 5 }} />
+          </TouchableHighlight>
+          <Text style={[
+            {
+              color: 'darkblue',
+              fontWeight: 'bold',
+              marginBottom: 0,
+              padding: 0,
+              fontSize: 20,
+              alignSelf: 'center',
+              marginTop: (Platform.OS === 'ios') ? 25 : 5,
+            }]}>
+            Recent Orders
+            </Text>
+          <TouchableHighlight
+            style={{
+              marginTop: (Platform.OS === 'ios') ? 25 : 5,
+              borderRadius: 20,
+            }}
+            underlayColor='#578dba' onPress={this.searchOrdersOnPress}>
+            <View style={{ marginRight: 8, flexDirection: 'row', justifyContent: 'center' }}>
+              <MaterialIcon name='search' color='white' size={25} style={{ alignSelf: 'center', marginLeft: 5, marginTop: 5, marginBottom: 5, marginRight: 5 }} />
+            </View>
+          </TouchableHighlight>
         </View>
-        <Text style={styles.title}>Recent Orders</Text>
         <ListView
           dataSource={this.state.dataSource}
           renderRow={this.renderRow}
