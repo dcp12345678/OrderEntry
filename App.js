f7320c6318a1a16ee983fdb4ae6f2a59fb48dc70
dcp12345678 <dcp12345678@yahoo.com>
2017-05-15 12:37:21 -0400

    Add App.js and combine android/ios index files
    Modified index.android.js and index.ios.js to just import
    App.s (newly added file).  So now, App.js is the common file
    used by both android and ios.

---
+++ App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+import MainApp from './components/MainApp';
+
+AppRegistry.registerComponent('OrderEntry', () => MainApp);
