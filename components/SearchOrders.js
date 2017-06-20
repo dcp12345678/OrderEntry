75d736875714da03bbdbc0cd98ea3e2b124dfae6
dcp12345678@yahoo.com <dcp12345678@yahoo.com>
2017-06-20 11:41:50 -0400

    Fetch orders using search criteria
    Modified SearchOrders.js to fetch orders using the search
    criteria. Also added isNullOrWhitespace to Helper.js

--- components/SearchOrders.js
+++ components/SearchOrders.js
@@ -26,6 +26,8 @@ import Spinner from 'react-native-loading-spinner-overlay';
 import DatePicker from 'react-native-datepicker';
 import moment from 'moment';
 
+const ordersApi = new OrdersApi();
+
 class SearchOrders extends Component {
 
   static navigationOptions = ({ navigation }) => ({
@@ -55,102 +57,119 @@ class SearchOrders extends Component {
     }
   }
 
-  searchOnPress = () => {
+  searchOnPress = async () => {
+    let criteria = {
+      createDateStart: this.state.createDateStart,
+      createDateEnd: this.state.createDateEnd
+    };
+    if (!Helper.isNullOrWhitespace(this.state.orderId)) {
+      criteria.orderId = this.state.orderId;
+    }
 
+    let res = await ordersApi.searchOrders(criteria);
+    let orders = JSON.parse(res.text);
+    this.setState({ orders });
   }
 
   render() {
-    return (
-      <View style={{ flex: 1, backgroundColor: 'lightsteelblue' }}>
-        <View style={{ margin: 10 }}>
-          <View>
-            <Text style={{ fontSize: 20, color: 'darkblue', fontWeight: 'bold', marginRight: 10 }}>Created between</Text>
-          </View>
-          <View style={{ marginTop: 10, flexDirection: 'row' }}>
-            <DatePicker
-              date={this.state.createDateStart}
-              mode="date"
-              placeholder=""
-              format="YYYY-MM-DD"
-              minDate="2010-05-01"
-              maxDate="2018-06-01"
-              confirmBtnText="Confirm"
-              cancelBtnText="Cancel"
-              showIcon={false}
-              customStyles={{
-                dateInput: {
-                  borderRadius: 7,
-                  backgroundColor: 'white',
-                },
-                dateText: {
-                  fontSize: (Platform.OS === 'ios') ? 20 : 18,
-                  backgroundColor: 'white',
-                  color: 'darkblue',
-                }
-              }}
-              onDateChange={(date) => { this.setState({ createDateStart: date }) }}
-            />
-            <Text style={{
-              fontSize: 20,
-              color: 'darkblue',
-              fontWeight: 'bold',
-              marginLeft: 10,
-              marginRight: 10,
-              alignSelf: 'center'
-            }}>and</Text>
-            <DatePicker
-              date={this.state.createDateEnd}
-              mode="date"
-              placeholder=""
-              format="YYYY-MM-DD"
-              minDate="2010-05-01"
-              maxDate="2018-06-01"
-              confirmBtnText="Confirm"
-              cancelBtnText="Cancel"
-              showIcon={false}
-              customStyles={{
-                dateInput: {
-                  borderRadius: 7,
-                  backgroundColor: 'white',
-                },
-                dateText: {
-                  fontSize: (Platform.OS === 'ios') ? 20 : 18,
-                  backgroundColor: 'white',
-                  color: 'darkblue',
-                }
-              }}
-              onDateChange={(date) => { this.setState({ createDateEnd: date }) }}
-            />
-          </View>
-
-          <View style={{ flexDirection: 'column', marginTop: 20 }}>
-            <Text style={{ fontSize: 20, color: 'darkblue', fontWeight: 'bold' }}>Order ID</Text>
-            <TextInput
-              style={{
-                height: 40,
-                fontSize: (Platform.OS === 'ios') ? 20 : 18,
-                alignSelf: 'stretch',
-                backgroundColor: 'white',
-                borderRadius: 7,
+    if (!_.isUndefined(this.state.orders)) {
+      return (
+        <View><Text>Got the orders!</Text></View>
+      );
+    } else {
+      return (
+        <View style={{ flex: 1, backgroundColor: 'lightsteelblue' }}>
+          <View style={{ margin: 10 }}>
+            <View>
+              <Text style={{ fontSize: 20, color: 'darkblue', fontWeight: 'bold', marginRight: 10 }}>Created between</Text>
+            </View>
+            <View style={{ marginTop: 10, flexDirection: 'row' }}>
+              <DatePicker
+                date={this.state.createDateStart}
+                mode="date"
+                placeholder=""
+                format="YYYY-MM-DD"
+                minDate="2010-05-01"
+                maxDate="2018-06-01"
+                confirmBtnText="Confirm"
+                cancelBtnText="Cancel"
+                showIcon={false}
+                customStyles={{
+                  dateInput: {
+                    borderRadius: 7,
+                    backgroundColor: 'white',
+                  },
+                  dateText: {
+                    fontSize: (Platform.OS === 'ios') ? 20 : 18,
+                    backgroundColor: 'white',
+                    color: 'darkblue',
+                  }
+                }}
+                onDateChange={(date) => { this.setState({ createDateStart: date }) }}
+              />
+              <Text style={{
+                fontSize: 20,
                 color: 'darkblue',
-                paddingLeft: 7,
-                marginTop: 10,
-              }}
-              underlineColorAndroid='transparent'
-              placeholder="Order ID or leave blank" />
-          </View >
-          <LinearGradient style={{ borderRadius: 5, alignSelf: 'stretch', marginTop: 40 }}
-            colors={['#4c669f', '#3b5998', '#192f6a']} >
-            <TouchableHighlight underlayColor='steelblue' onPress={this.searchOnPress}>
-              <View style={{ flexDirection: 'row', justifyContent: 'center' }}>
-                <MaterialIcon name='search' color='white' size={20} style={{ alignSelf: 'center', marginLeft: 5, marginTop: 5, marginBottom: 5 }} />
-                <Text style={{ fontSize: 20, color: 'white', marginLeft: 5, marginTop: 5, marginBottom: 5 }}>Search</Text>
-              </View>
-            </TouchableHighlight>
-          </LinearGradient>
+                fontWeight: 'bold',
+                marginLeft: 10,
+                marginRight: 10,
+                alignSelf: 'center'
+              }}>and</Text>
+              <DatePicker
+                date={this.state.createDateEnd}
+                mode="date"
+                placeholder=""
+                format="YYYY-MM-DD"
+                minDate="2010-05-01"
+                maxDate="2018-06-01"
+                confirmBtnText="Confirm"
+                cancelBtnText="Cancel"
+                showIcon={false}
+                customStyles={{
+                  dateInput: {
+                    borderRadius: 7,
+                    backgroundColor: 'white',
+                  },
+                  dateText: {
+                    fontSize: (Platform.OS === 'ios') ? 20 : 18,
+                    backgroundColor: 'white',
+                    color: 'darkblue',
+                  }
+                }}
+                onDateChange={(date) => { this.setState({ createDateEnd: date }) }}
+              />
+            </View>
+
+            <View style={{ flexDirection: 'column', marginTop: 20 }}>
+              <Text style={{ fontSize: 20, color: 'darkblue', fontWeight: 'bold' }}>Order ID</Text>
+              <TextInput
+                style={{
+                  height: 40,
+                  fontSize: (Platform.OS === 'ios') ? 20 : 18,
+                  alignSelf: 'stretch',
+                  backgroundColor: 'white',
+                  borderRadius: 7,
+                  color: 'darkblue',
+                  paddingLeft: 7,
+                  marginTop: 10,
+                }}
+                onChangeText={(orderId) => this.setState({ orderId })}
+                underlineColorAndroid='transparent'
+                placeholder="Order ID or leave blank" />
+            </View >
+            <LinearGradient style={{ borderRadius: 5, alignSelf: 'stretch', marginTop: 40 }}
+              colors={['#4c669f', '#3b5998', '#192f6a']} >
+              <TouchableHighlight underlayColor='steelblue' onPress={this.searchOnPress}>
+                <View style={{ flexDirection: 'row', justifyContent: 'center' }}>
+                  <MaterialIcon name='search' color='white' size={20} style={{ alignSelf: 'center', marginLeft: 5, marginTop: 5, marginBottom: 5 }} />
+                  <Text style={{ fontSize: 20, color: 'white', marginLeft: 5, marginTop: 5, marginBottom: 5 }}>Search</Text>
+                </View>
+              </TouchableHighlight>
+            </LinearGradient>
+          </View>
         </View>
-      </View>
-    );
+      );
+    }
   }
 };
 
